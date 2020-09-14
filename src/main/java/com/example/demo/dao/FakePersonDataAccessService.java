64c102efdc7317f6fe6213e8b4efa9d51e2e1680
Michael Spremulli <michaels.wpi@gmail.com>
2020-09-13 22:12:39 -0400

    created post Person and get all People requests

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -1,11 +1,13 @@
 package com.example.demo.dao;
 
 import com.example.demo.model.Person;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Repository("fakeDao")
 public class FakePersonDataAccessService implements PersonDao {
     private static List<Person> DB = new ArrayList<>();
 
@@ -16,7 +18,8 @@ public class FakePersonDataAccessService implements PersonDao {
     }
 
     @Override
-    public int addPerson(Person person) {
-        return 0;
+    public List<Person> selectAllPeople() {
+        return DB;
     }
+
 }
--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -2,7 +2,11 @@ package com.example.demo.service;
 
 import com.example.demo.dao.PersonDao;
 import com.example.demo.model.Person;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class PersonService {
     private final PersonDao personDao;
 
@@ -12,5 +16,8 @@ public class PersonService {
 
     public int addPerson(Person person){
         return personDao.insertPerson(person);
+    }
+    public List<Person> getAllPeople(){
+        return personDao.selectAllPeople();
     }
 }
